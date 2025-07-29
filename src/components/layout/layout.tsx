7603354f4211bcb18f3fd351cd6afc0e49a75765
HallefHLVieira <hallef.vieira@hotmail.com>
2025-07-28 22:52:54 -0300

    update server deploy

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -6,11 +6,11 @@ export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen bg-[url('/assets/images/bg_fultball.jpg')] bg-cover bg-center dark">
       <header className="bg-gray-900 text-white p-4">
-        <h1 className="text-xl">Vai ter jogo</h1>
+        <h1 className="text-xl">Tá valendo</h1>
       </header>
       <main className="flex-grow p-4">{children}</main>
       <footer className="bg-gray-900 text-white p-4 text-center">
-        © Vai ter jogo 2025
+        © Tá valendo 2025
       </footer>
     </div>
   )
